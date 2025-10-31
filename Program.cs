f73c15f04b0be102d942b174780087fd9d3b75a9
Rodrigo <rodrigocsilva291@gmail.com>
2025-10-31 20:41:13 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 0;
+int maximo;
+
+Console.Write("Digite o valor máximo: ");
+maximo = Convert.ToInt32(Console.ReadLine());
+
+while (n <= maximo)
+{
+    Console.ForegroundColor = (ConsoleColor)(n % 16);
+    Console.Write($"{n} ");
+    n = n + 1;
+    Thread.Sleep(200);
+}
+
+Console.ResetColor();
